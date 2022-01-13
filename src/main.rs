6b2dfc82377a20120649884cc35db9e70193f316
jyotsna-penumaka <jpenumak@redhat.com>
2022-01-13 13:36:28 -0500

    Create a PR Signed-off-by: jyotsna-penumaka <jpenumak@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -8,47 +8,69 @@ use anyhow::Result;
 use random_string::generate;
 
 fn main() -> Result<()> {
+    let owner = "jyotsna-penumaka";
+    let repo = "acme";
+    let base = "main";
+    let maintainer_can_modify = true;
+    let draft = false;
+    
     // get the SHA of the head
     let head_sha = api_client::get_base(
-        "lukehinds",
-        "acme",
-        "main"
+        owner,
+        repo,
+        base
     )?;
     println!("head_sha: {:?}", head_sha);
 
     // Generate random branch name
     let charset = "1234567890";
-    let generated_ref = format!("refs/heads/{}", generate(6, charset));
+    let branch_name = generate(6, charset);
+    let generated_ref = format!("refs/heads/{}", branch_name);
 
-    let get_ref = api_client::get_ref("lukehinds",
-        "acme",
+    let get_ref = api_client::get_ref(owner,
+        repo,
         generated_ref,
         head_sha.clone()
     )?;
     println!("get_ref: {:?}", get_ref);
 
     let tree_sha = api_client::create_tree(
-        "lukehinds",
-        "acme",
+        owner,
+        repo,
         head_sha.clone()
     )?;
     println!("tree_sha: {:?}", tree_sha);
 
-    // Returns a Vec<Content>
     let parents = api_client::get_parent(
-        "lukehinds",
-        "acme",
+        owner,
+        repo,
         head_sha.clone()
     )?;
-    // println!("parent: {:?}", parents[0].sha);
     // println!("parent: {:?}", parents[1].sha);
 
+    // Make a commit
     let commit_sha = api_client::push_commit(
-        "lukehinds",
-        "acme",
+        get_ref,
+        owner,
+        repo,
         tree_sha.clone(),
         parents
     )?;
     println!("commit_sha: {:?}", commit_sha);
+
+    // Create a pull request
+    let pr_title = format!("This pull request created by {} merges {} into {}", owner, branch_name, base);
+
+    let pr_url = api_client::create_pr(
+        owner, 
+        repo,
+        &pr_title, 
+        branch_name.to_string(), 
+        base.to_string(), 
+        maintainer_can_modify,
+        draft
+    )?;  
+    println!("pr_url: {:?}", pr_url);
+    
     Ok(())
 }
